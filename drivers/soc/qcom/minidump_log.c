0fe9467413395def601c4cba28d2eead637bff4b
Jian Cheng <chengj10@motorola.com>
2022-12-02 13:36:23 +0800

    minidump: reduce uncommonly used minidump files
    Remove percpu static sections (md_KSPERCPU*.BIN) and irq stack
    files (md_KISTK*_*.BIN) from minidump cause these files are
    uncommonly used for developmenting and debugging.
    
    Change-Id: I6b51a4ec66f4eaf886af1ad043d433134e4f04e9
    Signed-off-by: Jian Cheng <chengj10@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2449234
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/2782482
    Reviewed-by: Long Qian <qianl2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3041741

--- drivers/soc/qcom/minidump_log.c
+++ drivers/soc/qcom/minidump_log.c
@@ -183,7 +183,9 @@ static void register_kernel_sections(void)
 	char *rodata_name = "KROAIDATA";
 	size_t static_size;
 	void __percpu *base;
+#ifdef QCOM_DESIGN
 	unsigned int cpu;
+#endif
 	void *_sdata, *__bss_stop;
 	void *start_ro, *end_ro;
 
@@ -208,6 +210,7 @@ static void register_kernel_sections(void)
 	if (msm_minidump_add_region(&ksec_entry) < 0)
 		pr_err("Failed to add rodata section in Minidump\n");
 
+#ifdef QCOM_DESIGN
 	/* Add percpu static sections */
 	for_each_possible_cpu(cpu) {
 		void *start = per_cpu_ptr(base, cpu);
@@ -221,6 +224,7 @@ static void register_kernel_sections(void)
 		if (msm_minidump_add_region(&ksec_entry) < 0)
 			pr_err("Failed to add percpu sections in Minidump\n");
 	}
+#endif
 }
 
 static inline bool in_stack_range(
@@ -551,7 +555,7 @@ static void register_suspend_context(void)
 }
 #endif
 
-#ifdef CONFIG_ARM64
+#if defined(CONFIG_ARM64) && defined(QCOM_DESIGN)
 static void register_irq_stack(void)
 {
 	int cpu;
