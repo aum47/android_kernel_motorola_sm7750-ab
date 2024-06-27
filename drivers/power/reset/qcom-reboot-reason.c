19d02cc59f7a1eaa2964594918843168d070b215
qianlong <qianl2@motorola.com>
2024-06-27 17:00:41 +0800

    Add system restart/recovery panic mode
    System later insmode qcom-dload-mode.ko, so add feature
    to qcom-reboot-reason.ko for debug some early reboot issue.
    
    When set cmdline "qcom-reboot-reason.sys_restart_mode=panic",
    the kernel will enter into ramdump when it normal reset.
    When set cmdline "qcom-reboot-reason.sys_restart_mode=recovery_panic",
    the kernel will enter into ramdump when it recovery reset.
    
    Change-Id: I0cd5425149467d4d76b653ad28747af323a61480
    Signed-off-by: qianlong <qianl2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3012856
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/3041744

--- drivers/power/reset/qcom-reboot-reason.c
+++ drivers/power/reset/qcom-reboot-reason.c
@@ -17,6 +17,9 @@
 #include <linux/nvmem-consumer.h>
 #include <linux/input/qpnp-power-on.h>
 
+static char *sys_restart_mode = "NULL";
+module_param(sys_restart_mode, charp, 0644);
+
 struct qcom_reboot_reason {
 	struct device *dev;
 	struct notifier_block reboot_nb;
@@ -61,6 +64,17 @@ static int qcom_reboot_reason_reboot(struct notifier_block *this,
 		}
 	}
 
+	pr_info("%s: sys_restart_mode [%s]\n", __func__, sys_restart_mode);
+	if (!strcmp(sys_restart_mode, "panic")) {
+		/* Trigger a real panic on debug setting */
+		BUG();
+	}
+	if (!strcmp(sys_restart_mode, "recovery_panic")) {
+		if (!strcmp(cmd, "recovery")) {
+			/* Trigger a real panic on debug setting */
+			BUG();
+		}
+	}
 	return NOTIFY_OK;
 }
 
