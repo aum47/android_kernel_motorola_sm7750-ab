4d7e0a815fce2791ac0390e42a424bf965ed6a00
Xunjie Xu <xuxj12@motorola.com>
2024-12-09 15:52:08 +0800

    haptic: force enable haptic module
    Haptic disabled by other module.
    Force enable it before play
    
    Change-Id: Id9471e728f68d3f96898ac89f2bd052b06f87386
    Signed-off-by: Xunjie Xu <xuxj12@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3165841
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/misc/qcom-hv-haptics.c
+++ drivers/input/misc/qcom-hv-haptics.c
@@ -3243,6 +3243,7 @@ static int haptics_runtime_resume_get(struct haptics_chip *chip)
 	if (!is_haptics_runtime_pm_enabled(chip))
 		return 0;
 
+	haptics_module_enable(chip, true);
 	return pm_runtime_resume_and_get(chip->dev);
 }
 
@@ -7520,9 +7521,6 @@ static int __maybe_unused haptics_resume(struct device *dev)
 	}
 #endif
 
-	if (is_haptics_runtime_pm_enabled(chip))
-		return 0;
-
 	return haptics_module_enable(chip, true);
 }
 
@@ -7547,9 +7545,6 @@ static int __maybe_unused haptics_restore(struct device *dev)
 	rc = haptics_ds_resume_config(dev);
 	if (rc < 0)
 		return rc;
-
-	if (is_haptics_runtime_pm_enabled(chip))
-		return 0;
 
 	return haptics_module_enable(chip, true);
 
